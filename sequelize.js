95698aa65b7c834fe1eeb251304b44fe01a87c00
carolvaz98 <carolvas98@hotmail.com>
2023-08-15 09:50:57 -0300

    conexão com a porta 8080

--- sequelize.js
+++ sequelize.js
@@ -9,13 +9,4 @@ const sequelize = new Sequelize(database, user, password, {
   dialect: dialect,
 });
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Conexão com o banco de dados estabelecida com sucesso.');
-  })
-  .catch((err) => {
-    console.error('Erro ao conectar ao banco de dados:', err);
-  });
-
 module.exports = sequelize;
